556a36c8a58c7c14638b0c33f016af522ba7d679
ABDERRAZZAKELGHYATI <abdrazakghiyati2@gmail.com>
2020-12-02 14:08:35 -0800

    d

--- brief/b1/src/b1/Main.java
+++ brief/b1/src/b1/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("Hello, User");
 		
 		// Ask user to enter fullname
 //		showFullName();
@@ -26,24 +26,36 @@ public class Main {
 		String lastName = scan.nextLine();
 		
 		System.out.println("Hi, " + firstName + " " + lastName);
+		
+//		scan.close();
 	}
 	
 	// get hours from user
 	public static void getHours() {
-		int price = 20;
+		double price = 100;
+		int extraHours = 0;
+		double compensation = 0.0f;
 		
 		Scanner scan = new Scanner(System.in);
 		
 		System.out.println("Enter hours ?");
-		float hours = scan.nextFloat();
+		int totalHours = scan.nextInt();
 		
-		if (hours < 40) {
-			System.out.println("1 - You will get " + price * hours + " MAD");
+		if (totalHours < 40) {
+			System.out.println("1 - You will get " + totalHours*price + " MAD");
 		}
-		else if(hours >= 40 && hours <= 60) {
-			double extra = price + 0.5 * price;
-			System.out.println("2 - Your compensation is " + extra);
-			System.out.println("3 - The final price is " + (price*hours + extra));
+		else if(totalHours >= 40 && totalHours <= 60) {
+			extraHours = totalHours - 40;
+			compensation = extraHours * price;
+			int main = totalHours - extraHours;
+			
+			System.out.println("Total hours: " +totalHours);
+			System.out.println("Extra hours: " + extraHours);
+			System.out.println("main hours: " + main);
+			System.out.println("compensation: " + compensation);
+			
+			System.out.println("Your compensation is " + compensation);
+			System.out.println("The final price is " + ((price*main) + compensation));
 		}else {
 			System.out.println("4 - Incorrect info");
 		}
